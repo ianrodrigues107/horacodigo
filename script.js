15b7d342ef4f41500212e11128b2a34acfc0e5f4
ianrodrigues107 <105176546+ianrodrigues107@users.noreply.github.com>
2022-05-08 18:51:36 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+const list = document.querySelectorAll('.list')
+function activeLink(){
+  list.forEach((item) => item.classList.remove('active'))
+  this.classList.add('active')
+}
+  list.forEach((item) =>
+  item.addEventListener('click', activeLink))
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+  const titulo = document.querySelector('h1');
+  typeWriter(titulo);
